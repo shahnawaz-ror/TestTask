c7a184a898ca23043c72c9884585038253d699de
mohammad shahnawaz <shahnawaz.spaceo@gmail.com>
2022-05-21 14:54:45 +0530

    Updated the user reward

--- db/migrate/20220521092235_create_user_rewards.rb
+++ db/migrate/20220521092235_create_user_rewards.rb
@@ -3,6 +3,7 @@ class CreateUserRewards < ActiveRecord::Migration[6.1]
     create_table :user_rewards do |t|
       t.references :user, null: false, foreign_key: true
       t.references :reward, null: false, foreign_key: true
+      t.boolean :status, default: false
 
       t.timestamps
     end
