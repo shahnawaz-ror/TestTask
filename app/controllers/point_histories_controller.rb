1c0059c4d5302253c743d238e072f9b63e78e3e1
Mohammad Shahnawaz <shahnawazkits@gmail.com>
2022-05-23 20:55:09 +0530

    Update point_histories_controller.rb

--- app/controllers/point_histories_controller.rb
+++ app/controllers/point_histories_controller.rb
@@ -4,6 +4,6 @@ class PointHistoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @point_histories = PointHistory.all
+    @point_histories = PointHistory.where(user_id: params[:user_id])  
   end
 end
